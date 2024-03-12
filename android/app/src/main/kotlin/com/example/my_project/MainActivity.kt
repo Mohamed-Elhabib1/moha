7fe95b5a6d64aa034117578e1bf298fea3b537ec
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-03-12 04:17:25 +0000

    Updating to latest FlutterFlow output.

---
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mycompany.moha
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/index.dart
@@ -0,0 +1,2 @@
+// Export pages
+export '/auth1/auth1_widget.dart' show Auth1Widget;
